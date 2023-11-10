a22877c2e9e4900952d6677488d0b5183ee2deec
nVen0m1 <150270305+nVen0m1@users.noreply.github.com>
2023-11-10 09:12:52 -0800

    Update main.py
    Here is the final main.py to work with all of the new States that were added.

--- main.py
+++ main.py
@@ -1,30 +1,49 @@
-import handlers
+import shop_bot
 from telegram.ext import (
-    CommandHandler, CallbackContext,
-    ConversationHandler, MessageHandler,
-    Filters, Updater, CallbackQueryHandler
+    CommandHandler,
+    CallbackContext,
+    ConversationHandler,
+    MessageHandler,
+    Filters,
+    Updater,
+    CallbackQueryHandler
 )
-from config import TOKEN
+from config import TOKEN, api_key, api_secret, FAUNA_KEY
 
-updater = Updater(token=TOKEN, use_context=True)
-print(updater)
+updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
-
 
 def main():
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('start', handlers.start)],
+        entry_points=[CommandHandler('start', shop_bot.start)],
         states={
-            handlers.CHOOSING: [
-                MessageHandler(
-                    Filters.all, handlers.choose
-                )
+            shop_bot.CHOOSING: [
+                MessageHandler(Filters.all, shop_bot.choose)
             ],
-            handlers.CLASS_STATE: [
-                CallbackQueryHandler(handlers.classer)
+            shop_bot.CLASS_STATE: [
+                CallbackQueryHandler(shop_bot.classer)
+            ],
+            shop_bot.SME_DETAILS: [
+                MessageHandler(Filters.all, shop_bot.business_details)
+            ],
+            shop_bot.ADD_PRODUCTS: [
+                CallbackQueryHandler(shop_bot.add_product),
+                MessageHandler(Filters.all, shop_bot.product_info)
+            ],
+            shop_bot.SME_CAT: [
+                CallbackQueryHandler(shop_bot.business_details_update)
+            ],
+            shop_bot.CHOOSE_PREF: [
+                CallbackQueryHandler(shop_bot.customer_pref)
+            ],
+            shop_bot.SHOW_STOCKS: [
+                CallbackQueryHandler(shop_bot.show_products)
+            ],
+            shop_bot.POST_VIEW_PRODUCTS: [
+                CallbackQueryHandler(shop_bot.post_view_products)
             ]
         },
-        fallbacks=[CommandHandler('cancel', handlers.cancel)],
+        fallbacks=[CommandHandler('cancel', shop_bot.cancel)],
         allow_reentry=True
     )
     dispatcher.add_handler(conv_handler)
