801599a8fa0ef78218f3fd4771389d74e338ea83
nVen0m1 <150270305+nVen0m1@users.noreply.github.com>
2023-11-13 19:21:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
+from telegram.ext import CommandHandler, ConversationHandler, MessageHandler, Filters, CallbackQueryHandler, Updater
 import shop_bot
-from telegram.ext import (
-    CommandHandler,
-    CallbackContext,
-    ConversationHandler,
-    MessageHandler,
-    Filters,
-    Updater,
-    CallbackQueryHandler
-)
-from config import TOKEN, api_key, api_secret, FAUNA_KEY
+from config import TOKEN
 
 updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -18,23 +10,26 @@ def main():
         entry_points=[CommandHandler('start', shop_bot.start)],
         states={
             shop_bot.CHOOSING: [
-                MessageHandler(Filters.all, shop_bot.choose)
+                MessageHandler(Filters.text & ~Filters.command, shop_bot.choose)
             ],
             shop_bot.CLASS_STATE: [
                 CallbackQueryHandler(shop_bot.classer)
             ],
-            shop_bot.SME_DETAILS: [
-                MessageHandler(Filters.all, shop_bot.business_details)
+            shop_bot.AWAIT_PASSWORD: [
+                MessageHandler(Filters.text & ~Filters.command, shop_bot.await_password)
             ],
-            shop_bot.ADD_PRODUCTS: [
-                CallbackQueryHandler(shop_bot.add_product),
-                MessageHandler(Filters.all, shop_bot.product_info)
+            shop_bot.SME_DETAILS: [
+                MessageHandler(Filters.text & ~Filters.command, shop_bot.business_details)
+            ],
+            shop_bot.CHOOSE_PREF: [
+                CallbackQueryHandler(shop_bot.customer_pref)
             ],
             shop_bot.SME_CAT: [
                 CallbackQueryHandler(shop_bot.business_details_update)
             ],
-            shop_bot.CHOOSE_PREF: [
-                CallbackQueryHandler(shop_bot.customer_pref)
+            shop_bot.ADD_PRODUCTS: [
+                CallbackQueryHandler(shop_bot.add_product),
+                MessageHandler(Filters.photo | Filters.text, shop_bot.product_info)
             ],
             shop_bot.SHOW_STOCKS: [
                 CallbackQueryHandler(shop_bot.show_products)
@@ -46,6 +41,7 @@ def main():
         fallbacks=[CommandHandler('cancel', shop_bot.cancel)],
         allow_reentry=True
     )
+
     dispatcher.add_handler(conv_handler)
     updater.start_polling()
     updater.idle()
