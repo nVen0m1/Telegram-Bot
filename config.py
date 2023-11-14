5091a2dfd2a20058971541a46675b3d233486d26
nVen0m1 <150270305+nVen0m1@users.noreply.github.com>
2023-11-13 19:21:36 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+TOKEN = os.environ['BOT_TOKEN']
+api_secret = os.environ['API_SECRET']
+api_key = os.environ['API_KEY']
+FAUNA_KEY = os.environ['FAUNA_KEY']
+dealer_password = os.environ['DEALER_PASSWORD']
